e75addeafe9366d213711c0db4c22c4b674895a4
base0 <vacapp@live.com>
2018-11-29 15:18:35 +0700

    Add files via upload

--- basic_webhook.py
+++ basic_webhook.py
@@ -1,8 +1,34 @@
-from flask import Flask, request
 
-VERIFY_TOKEN = 'xxx'
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+from flask import request as frequest
+from urllib import request as urequest
+from urllib import parse
+
+VERIFY_TOKEN = ''
+ACCESS_TOKEN = ''
 
 app = Flask(__name__)
+
+def chat(uid, msg):
+    host = 'https://graph.facebook.com/v2.6/me/'
+    path = 'messages/'
+    d = {
+	  'access_token' : ACCESS_TOKEN,
+      "messaging_type": "UPDATE",
+      "recipient": {
+        "id": uid
+      },
+      "message": {
+        "text": msg
+      }
+    }
+    url = host + path
+
+    data = parse.urlencode(d).encode()
+    req =  urequest.Request(url, data=data) # this will make the method "POST"
+    resp = urequest.urlopen(req)
 
 @app.route('/')
 def hello_world():
@@ -10,16 +36,16 @@ def hello_world():
 
 @app.route('/webhook', methods=['GET'])
 def handle_verification():
-    if (request.args.get('hub.verify_token', '') == VERIFY_TOKEN):
-        print("Verified")
-        return request.args.get('hub.challenge', '')
+    if (frequest.args.get('hub.verify_token', '') == VERIFY_TOKEN):
+        print('Verified')
+        return frequest.args.get('hub.challenge', '')
     else:
-        print("Wrong token")
-        return "Error, wrong validation token"
+        print('Wrong token')
+        return 'Error, wrong validation token'
 
 @app.route('/webhook', methods=['POST'])
 def handle_message():
-    data = request.get_json()
+    data = frequest.get_json()
     print(data)
 
     if data['object'] == 'page':
@@ -29,7 +55,6 @@ def handle_message():
                     sender_id = messaging_event['sender']['id']
                     recipient_id = messaging_event['recipient']['id']
                     message_text = messaging_event['message']['text']
-                    # send_message_response(sender_id, parse_natural_text(message_text))
-
-
+                    chat(sender_id, message_text)
     return 'ok'
+
